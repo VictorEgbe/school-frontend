79f5ec125e7775ce4710b2f451dc230068c22367
unknown <victoregbejunior@gmail.com>
2023-06-21 13:36:34 +0100

    Absences page created

--- src/pages/absences/Absences.jsx
+++ src/pages/absences/Absences.jsx
@@ -1,7 +1,44 @@
 import './Absences.scss'
+import { classes } from './data'
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+import ManIcon from '@mui/icons-material/Man'
+import WomanIcon from '@mui/icons-material/Woman'
+import { Link } from 'react-router-dom'
 
 const Absences = () => {
-  return <div className="absences">Absences</div>
+  return (
+    <div className="absences">
+      <div className="wrapper">
+        <div className="info">
+          <p className="select">Select a class to input the absences</p>
+          <div title="Add a student" className="add">
+            <AddCircleIcon />
+          </div>
+        </div>
+
+        <div className="classesCardsContainer">
+          {classes.map((c) => (
+            <Link
+              to={`/absences/${c.id}`}
+              className="classCard link"
+              key={c.id}
+            >
+              <div className="topClass">{c.name}</div>
+              <div className="bottomClass">
+                <span>
+                  <ManIcon /> {c.boys}
+                </span>
+                <span>
+                  <WomanIcon /> {c.girls}
+                </span>
+                <span>Total: {c.total}</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Absences
