eb54c77fcb7d6a46cc07eefa4f6d6999356c2216
unknown <victoregbejunior@gmail.com>
2023-06-22 18:10:30 +0100

    Add subject page without dynamic input

--- src/pages/class_subjects/ClassSubject.jsx
+++ src/pages/class_subjects/ClassSubject.jsx
@@ -1,7 +1,107 @@
 import './ClassSubject.scss'
+import { coefficients, subjects, teachers } from './data'
+import AddIcon from '@mui/icons-material/Add'
 
 const ClassSubject = () => {
-  return <div className="classSubject">Class Subject</div>
+  return (
+    <div className="classSubject">
+      <div className="wrapper">
+        <div className="info">Upper Sixth Science</div>
+        <section>
+          <div className="sectionWrapper">
+            <div className="subject">
+              <select>
+                <option disabled selected>
+                  --Select the subject--
+                </option>
+                {subjects.map((subject) => (
+                  <option key={subject.id}>{subject.name}</option>
+                ))}
+              </select>
+              <AddIcon
+                className="icon"
+                sx={{ fontSize: '1.8rem' }}
+                titleAccess="CREATE SUBJECT"
+              />
+            </div>
+
+            <div className="coefficient">
+              <select>
+                <option disabled selected>
+                  --Coefficient--
+                </option>
+                {coefficients.map((c) => (
+                  <option key={c.id}>{c.coefficient}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="teacher">
+              <select>
+                <option disabled selected>
+                  --Select the teacher--
+                </option>
+                {teachers.map((t) => (
+                  <option key={t.id}>{t.name}</option>
+                ))}
+              </select>
+              <AddIcon
+                className="icon"
+                sx={{ fontSize: '1.8rem' }}
+                titleAccess="CREATE TEACHER"
+              />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="sectionWrapper">
+            <div className="subject">
+              <select>
+                <option disabled selected>
+                  --Select the subject--
+                </option>
+                {subjects.map((subject) => (
+                  <option key={subject.id}>{subject.name}</option>
+                ))}
+              </select>
+              <AddIcon
+                className="icon"
+                sx={{ fontSize: '1.8rem' }}
+                titleAccess="CREATE SUBJECT"
+              />
+            </div>
+
+            <div className="coefficient">
+              <select>
+                <option disabled selected>
+                  --Coefficient--
+                </option>
+                {coefficients.map((c) => (
+                  <option key={c.id}>{c.coefficient}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="teacher">
+              <select>
+                <option disabled selected>
+                  --Select the teacher--
+                </option>
+                {teachers.map((t) => (
+                  <option key={t.id}>{t.name}</option>
+                ))}
+              </select>
+              <AddIcon
+                className="icon"
+                sx={{ fontSize: '1.8rem' }}
+                titleAccess="CREATE TEACHER"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  )
 }
 
 export default ClassSubject
---
+++ src/pages/class_subjects/data.js
@@ -0,0 +1,72 @@
+export const subjects = [
+  {
+    id: '1',
+    name: 'Mathematics',
+  },
+  {
+    id: '2',
+    name: 'English Language',
+  },
+  {
+    id: '2',
+    name: 'Physics',
+  },
+  {
+    id: '4',
+    name: 'Economics',
+  },
+  {
+    id: '5',
+    name: 'History',
+  },
+  {
+    id: '6',
+    name: 'Additional Mathematics',
+  },
+]
+
+export const coefficients = [
+  {
+    id: 1,
+    coefficient: 1,
+  },
+  {
+    id: 2,
+    coefficient: 2,
+  },
+  {
+    id: 3,
+    coefficient: 3,
+  },
+  {
+    id: 4,
+    coefficient: 4,
+  },
+  {
+    id: 5,
+    coefficient: 5,
+  },
+]
+
+export const teachers = [
+  {
+    id: 76,
+    name: 'Mina Fields',
+  },
+  {
+    id: 17,
+    name: 'Gordon Sanders',
+  },
+  {
+    id: 41,
+    name: 'Terry Sullivan',
+  },
+  {
+    id: 14,
+    name: 'Terry Goodman',
+  },
+  {
+    id: 9,
+    name: 'Eric Castillo',
+  },
+]
