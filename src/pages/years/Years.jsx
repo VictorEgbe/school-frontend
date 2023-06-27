243bcad7089cdc9e2cf5ec4ea54019e5ebdb80e8
unknown <victoregbejunior@gmail.com>
2023-06-27 15:27:23 +0100

    Years page done

--- src/pages/years/Years.jsx
+++ src/pages/years/Years.jsx
@@ -1,7 +1,65 @@
 import './Years.scss'
-
+import { years } from './data'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import CancelIcon from '@mui/icons-material/Cancel'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import ToggleOffIcon from '@mui/icons-material/ToggleOff'
+import AddIcon from '@mui/icons-material/Add'
 const Years = () => {
-  return <div className="years">Years</div>
+  return (
+    <div className="years">
+      <div className="container">
+        <section className="create" title="Create a New Year">
+          <AddIcon className="icon" />
+        </section>
+        <table>
+          <thead>
+            <tr>
+              <th>Year</th>
+              <th>Active?</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          {years.map((year) => (
+            <tr key={year.id}>
+              <td>{year.name}</td>
+              <td>
+                {year.isActive ? (
+                  <CheckCircleIcon
+                    sx={{ color: '#1b5e20' }}
+                    titleAccess="Active"
+                  />
+                ) : (
+                  <CancelIcon
+                    sx={{ color: '#d50000' }}
+                    titleAccess="Inactive"
+                  />
+                )}
+              </td>
+              {year.isActive && (
+                <td className="actions">
+                  <EditIcon
+                    sx={{ color: 'blue', cursor: 'pointer' }}
+                    titleAccess="Edit year"
+                  />
+                  <ToggleOffIcon
+                    sx={{ color: 'green', cursor: 'pointer' }}
+                    titleAccess="Deactivate year"
+                  />
+                  <DeleteIcon
+                    sx={{ color: 'red', cursor: 'pointer' }}
+                    titleAccess="Delete Year"
+                  />
+                </td>
+              )}
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
+  )
 }
 
 export default Years
---
+++ src/pages/years/data.js
@@ -0,0 +1,57 @@
+export const years = [
+  {
+    id: 1,
+    name: '2023-2024',
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: '2022-2023',
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: '2021-2022',
+    isActive: false,
+  },
+
+  {
+    id: 4,
+    name: '2020-2021',
+    isActive: false,
+  },
+  {
+    id: 5,
+    name: '2020-2021',
+    isActive: false,
+  },
+  {
+    id: 6,
+    name: '2019-2020',
+    isActive: false,
+  },
+
+  {
+    id: 7,
+    name: '2018-2019',
+    isActive: false,
+  },
+
+  {
+    id: 8,
+    name: '2017-2018',
+    isActive: false,
+  },
+
+  {
+    id: 9,
+    name: '2016-2017',
+    isActive: false,
+  },
+
+  {
+    id: 10,
+    name: '2015-2016',
+    isActive: false,
+  },
+]
