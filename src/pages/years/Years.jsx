f12351ecab6f8aa509591e4ef745a10dc8dff17c
unknown <victoregbejunior@gmail.com>
2023-06-27 16:53:40 +0100

    Added Terms and Sequences on years page

--- src/pages/years/Years.jsx
+++ src/pages/years/Years.jsx
@@ -1,5 +1,5 @@
 import './Years.scss'
-import { years } from './data'
+import { years, terms, sequences } from './data'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 import CancelIcon from '@mui/icons-material/Cancel'
 import EditIcon from '@mui/icons-material/Edit'
@@ -10,17 +10,30 @@ const Years = () => {
   return (
     <div className="years">
       <div className="container">
-        <table>
+        <div className="actionSection">
+          <div className="add">
+            <AddIcon /> Year
+          </div>
+          <div className="add">
+            <AddIcon /> Term
+          </div>
+          <div className="add">
+            <AddIcon /> Sequence
+          </div>
+        </div>
+        <table className="yearsTable">
           <thead>
             <tr>
+              <th>S/N</th>
               <th>Years</th>
               <th>Active?</th>
               <th>Actions</th>
             </tr>
           </thead>
 
-          {years.map((year) => (
+          {years.map((year, index) => (
             <tr key={year.id}>
+              <td>{index + 1}</td>
               <td>{year.name}</td>
               <td>
                 {year.isActive ? (
@@ -56,8 +69,110 @@ const Years = () => {
         </table>
 
         <div className="termSequenceWrapper">
-          <div className="terms">Terms</div>
-          <div className="sequences">Sequences</div>
+          <div className="terms">
+            <p>Terms</p>
+            <table className="table">
+              <colgroup>
+                <col style={{ width: 'maxContent' }}></col>
+                <col></col>
+                <col></col>
+                <col style={{ width: '40%' }}></col>
+              </colgroup>
+              <tr>
+                <th>s/n</th>
+                <th>Name</th>
+                <th>IsActive?</th>
+                <th>Actions</th>
+              </tr>
+              {terms.map((term, index) => (
+                <tr key={term.id}>
+                  <td>{index + 1}</td>
+                  <td>{term.name}</td>
+                  <td>
+                    {term.isActive ? (
+                      <CheckCircleIcon
+                        sx={{ color: '#1b5e20' }}
+                        titleAccess="Active"
+                      />
+                    ) : (
+                      <CancelIcon
+                        sx={{ color: '#d50000' }}
+                        titleAccess="Inactive"
+                      />
+                    )}
+                  </td>
+                  {term.isActive && (
+                    <td className="actions">
+                      <EditIcon
+                        sx={{ color: 'blue', cursor: 'pointer' }}
+                        titleAccess="Edit term"
+                      />
+                      <ToggleOffIcon
+                        sx={{ color: 'green', cursor: 'pointer' }}
+                        titleAccess="Deactivate term"
+                      />
+                      <DeleteIcon
+                        sx={{ color: 'red', cursor: 'pointer' }}
+                        titleAccess="Delete term"
+                      />
+                    </td>
+                  )}
+                </tr>
+              ))}
+            </table>
+          </div>
+          <div className="sequences">
+            <p>Sequences</p>
+            <table className="table">
+              <colgroup>
+                <col style={{ width: 'maxContent' }}></col>
+                <col></col>
+                <col></col>
+                <col style={{ width: '40%' }}></col>
+              </colgroup>
+              <tr>
+                <th>s/n</th>
+                <th>Name</th>
+                <th>IsActive?</th>
+                <th>Actions</th>
+              </tr>
+              {sequences.map((sequence, index) => (
+                <tr key={sequence.id}>
+                  <td>{index + 1}</td>
+                  <td>{sequence.name}</td>
+                  <td>
+                    {sequence.isActive ? (
+                      <CheckCircleIcon
+                        sx={{ color: '#1b5e20' }}
+                        titleAccess="Active"
+                      />
+                    ) : (
+                      <CancelIcon
+                        sx={{ color: '#d50000' }}
+                        titleAccess="Inactive"
+                      />
+                    )}
+                  </td>
+                  {sequence.isActive && (
+                    <td className="actions">
+                      <EditIcon
+                        sx={{ color: 'blue', cursor: 'pointer' }}
+                        titleAccess="Edit sequence"
+                      />
+                      <ToggleOffIcon
+                        sx={{ color: 'green', cursor: 'pointer' }}
+                        titleAccess="Deactivate sequence"
+                      />
+                      <DeleteIcon
+                        sx={{ color: 'red', cursor: 'pointer' }}
+                        titleAccess="Delete sequence"
+                      />
+                    </td>
+                  )}
+                </tr>
+              ))}
+            </table>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/years/data.js
+++ src/pages/years/data.js
@@ -26,3 +26,34 @@ export const years = [
     isActive: false,
   },
 ]
+
+export const terms = [
+  {
+    id: 1,
+    name: 'Third term',
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: 'Second term',
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: 'First term',
+    isActive: false,
+  },
+]
+
+export const sequences = [
+  {
+    id: 1,
+    name: 'Second Sequence',
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: 'First Sequence',
+    isActive: false,
+  },
+]
