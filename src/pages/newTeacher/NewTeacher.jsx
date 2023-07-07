9d4b0028067e4cd27acf0ef0d19038c0e68abc42
unknown <victoregbejunior@gmail.com>
2023-07-07 17:49:38 +0100

    Add teacher page designed

--- src/pages/newTeacher/NewTeacher.jsx
+++ src/pages/newTeacher/NewTeacher.jsx
@@ -1,7 +1,95 @@
 import './NewTeacher.scss'
+import Avatar from '../../assets/avatar.png'
+import Folder from '@mui/icons-material/Folder'
+import { Send } from '@mui/icons-material'
+import { userInput } from './data'
+import { useState } from 'react'
 
 const NewTeacher = () => {
-  return <div className="newTeacher">New Teacher</div>
+  const [image, setImage] = useState(null)
+  const [gender, setGender] = useState('Male')
+  const [inputInfo, setInputInfo] = useState({
+    first_name: '',
+    last_name: '',
+    phone: '',
+    address: '',
+    email: '',
+    username: '',
+    date_of_birth: '',
+    password: '',
+    password2: '',
+  })
+
+  const handleChange = (e) => {
+    setInputInfo({ ...inputInfo, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = { ...inputInfo, gender, image }
+    console.log(data)
+    // Make api call here
+  }
+
+  return (
+    <div className="newTeacher">
+      <div className="container">
+        <h1>Add A New Teacher</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="left">
+            <img src={image ? URL.createObjectURL(image) : Avatar} alt="" />
+
+            <div>
+              <label className="folder" htmlFor="image">
+                Select an image: <Folder />
+              </label>
+              <input
+                required
+                id="image"
+                type="file"
+                accept=".png, .jpg, .jpeg"
+                style={{ display: 'none' }}
+                onChange={(e) => setImage(e.target.files[0])}
+              />
+            </div>
+          </div>
+          <div className="right">
+            {userInput.map((input, index) => (
+              <div className="formGroup" key={index}>
+                <label htmlFor={input.id}>{input.label}</label>
+                <input
+                  required
+                  id={input.id}
+                  type={input.type}
+                  placeholder={input.placeholder}
+                  onChange={handleChange}
+                  name={input.id}
+                />
+              </div>
+            ))}
+
+            <div className="formGroup">
+              <label htmlFor="gender">Gender: </label>
+              <select
+                required
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <option>Male</option>
+                <option>Female</option>
+              </select>
+            </div>
+
+            <div className="button">
+              <button type="submit">
+                <Send /> Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default NewTeacher
---
+++ src/pages/newTeacher/data.js
@@ -0,0 +1,63 @@
+export const userInput = [
+  {
+    id: 'first_name',
+    type: 'text',
+    label: 'First Name',
+    placeholder: 'e.g John',
+  },
+
+  {
+    id: 'last_name',
+    type: 'text',
+    label: 'Last Name',
+    placeholder: 'e.g Doe',
+  },
+  {
+    id: 'phone',
+    type: 'tel',
+    label: 'Phone Number',
+    placeholder: 'e.g 666777888',
+  },
+
+  {
+    id: 'address',
+    type: 'text',
+    label: 'Address',
+    placeholder: 'e.g TKC, Yaounde',
+  },
+
+  {
+    id: 'email',
+    type: 'email',
+    label: 'Email',
+    placeholder: 'e.g johndoe@gmail.cm',
+  },
+
+  {
+    id: 'username',
+    type: 'text',
+    label: 'Username',
+    placeholder: 'e.g JohnDoe',
+  },
+
+  {
+    id: 'date_of_birth',
+    type: 'date',
+    label: 'Date of',
+    placeholder: 'e.g select DOB',
+  },
+
+  {
+    id: 'password',
+    type: 'password',
+    label: 'Password',
+    placeholder: '',
+  },
+
+  {
+    id: 'password2',
+    type: 'password',
+    label: 'Enter Confirmation',
+    placeholder: '',
+  },
+]
