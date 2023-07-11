7ede8523ae1e200258d0d80b0e4f16692c83b812
unknown <victoregbejunior@gmail.com>
2023-07-11 10:18:59 +0100

    Fetching single teacher and displaying the data correctly completed

--- src/pages/teacher/Teacher.jsx
+++ src/pages/teacher/Teacher.jsx
@@ -1,10 +1,40 @@
 import './Teacher.scss'
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
+import { useQuery } from '@tanstack/react-query'
 import { Link, useParams } from 'react-router-dom'
+import { authCall } from '../../apiCalls/index'
+import Error from '../../components/Error/Error'
+import noAvatar from '../../assets/avatar.png'
+import Spinner from '../../components/loadingSpinner/Spinner'
 
 const Teacher = () => {
   const { teacherID } = useParams()
+
+  const {
+    isLoading,
+    isError,
+    data: teacher,
+  } = useQuery({
+    queryKey: ['teacher', teacherID],
+    queryFn: () =>
+      authCall.get(`teachers/get_teacher/${teacherID}`).then((res) => res.data),
+  })
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    const errorMsg = 'Something went wrong. Please reload page.'
+    return <Error errorMsg={errorMsg} />
+  }
+
+  const { teacherInfo, classes, similarTeachers } = teacher
+
+  function truncate(str, maxlength) {
+    return str.length > maxlength ? str.slice(0, maxlength - 1) + '…' : str
+  }
 
   return (
     <div className="teacher">
@@ -30,24 +60,27 @@ const Teacher = () => {
             <div className="bottomLeft">
               <div className="imageContainer">
                 <img
-                  src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  src={teacherInfo.image ? teacherInfo.image : noAvatar}
                   alt=""
                 />
               </div>
               <div className="otherInfos">
-                <h1>Helen McMoli Enoh</h1>
+                <h1>{teacherInfo.full_name}</h1>
 
                 <div className="others">
-                  <p className="age">Age: 26 years old</p>
-                  <p className="department">Depart: Mathematics</p>
-                  <p className="gender">Gender: Male</p>
-                  <p className="gender">H.O.D: NO</p>
-                  <p className="gender">Married: NO</p>
-                  <p className="address">Address: Yaounde</p>
-                  <p className="DOB">D.O.B: 26/march/2023</p>
-                  <p className="email">Email: ki@sih.vu</p>
-                  <p className="tel">Phone: 675725140</p>
-                  <p className="username">Username: JW</p>
+                  <p className="age">Age: {teacherInfo.age} years old</p>
+                  <p className="department">Depart: {teacherInfo.department}</p>
+                  <p className="gender">Gender: {teacherInfo.gender}</p>
+                  <p className="gender">
+                    H.O.D: {teacherInfo.isHOD ? 'Yes' : 'No'}{' '}
+                  </p>
+                  <p className="address">Address: {teacherInfo.address} </p>
+                  <p className="DOB">D.O.B: {teacherInfo.date_of_birth}</p>
+                  <p className="email">Email: {teacherInfo.email} </p>
+                  <p className="tel">Phone: {teacherInfo.phone} </p>
+                  {teacherInfo.username && (
+                    <p className="username">Username: {teacherInfo.username}</p>
+                  )}
                 </div>
               </div>
             </div>
@@ -55,146 +88,52 @@ const Teacher = () => {
           <div className="right">
             <div className="rightContainer">
               <h1>
-                Classes assigned to <span>Helen McMoli Enoh</span>{' '}
+                Classes assigned to <span>{teacherInfo.full_name}</span>{' '}
               </h1>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Class</th>
-                    <th>Subject</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  <tr>
-                    <td>Form 1</td>
-                    <td>History</td>
-                  </tr>
-                  <tr>
-                    <td>Form 2</td>
-                    <td>History</td>
-                  </tr>
-                  <tr>
-                    <td>Form 3</td>
-                    <td>History</td>
-                  </tr>
-                  <tr>
-                    <td>Form 4</td>
-                    <td>History</td>
-                  </tr>
-                  <tr>
-                    <td>Form 5</td>
-                    <td>History</td>
-                  </tr>
-                </tbody>
-              </table>
+              {classes.length === 0 ? (
+                <h2>None yet.</h2>
+              ) : (
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Class</th>
+                      <th>Subject</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {classes.map((c, index) => (
+                      <tr key={index}>
+                        <td>{c.NameOfClass}</td>
+                        <td>{c.subjectTaught}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
             </div>
           </div>
         </section>
         <section className="bottom">
           <h1>Teachers of the same department (Similar Teachers)</h1>
-          <div className="similarContainer">
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
+          {similarTeachers.length === 0 ? (
+            <h2 className="noTeachers">No other teachers in the department</h2>
+          ) : (
+            <div className="similarContainer">
+              {similarTeachers.map((t) => (
+                <Link
+                  to={`/teachers/${t.id}`}
+                  className="similarCard link"
+                  key={t.id}
+                >
+                  <img src={t.image ? t.image : noAvatar} alt="" />
+                  <div>
+                    {truncate(`${t.name}`, 15)}{' '}
+                    {t.isHOD && <span>(H.O.D)</span>}
+                  </div>
+                </Link>
+              ))}
             </div>
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
-            </div>
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
-            </div>
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
-            </div>
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
-            </div>
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
-            </div>
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
-            </div>
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
-            </div>
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
-            </div>
-            <div className="similarCard">
-              <img
-                src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt=""
-              />
-
-              <div>
-                John Doe <span>(H.O.D)</span>
-              </div>
-            </div>
-          </div>
+          )}
         </section>
       </div>
     </div>
