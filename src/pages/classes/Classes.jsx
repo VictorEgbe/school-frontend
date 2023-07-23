e87bdade80a3b476578a5293b885abbbb10787f4
unknown <victoregbejunior@gmail.com>
2023-07-23 16:11:32 +0100

    Fetching of classes and display correctly

--- src/pages/classes/Classes.jsx
+++ src/pages/classes/Classes.jsx
@@ -1,36 +1,57 @@
 import './Classes.scss'
-import { classes } from './data'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
-import ManIcon from '@mui/icons-material/Man'
-import WomanIcon from '@mui/icons-material/Woman'
 import { Link } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { authCall } from '../../apiCalls/index'
+import Spinner from '../../components/loadingSpinner/Spinner'
+import Error from '../../components/Error/Error'
 
 const Classes = () => {
+  const {isError, isLoading, data} = useQuery({
+    queryKey: ['classes'],
+    queryFn: () => authCall.get('classes').then(res => res.data),
+    retry: 2
+  })
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    const errorMsg = 'Something went wrong. Please reload the page.'
+    return <Error errorMsg={errorMsg} />
+  }
+
   return (
     <div className="classes">
       <div className="wrapper">
-        <div title="Add a class" className="add">
-          <AddCircleIcon />
+        <div className="header">
+          <div className="basicInfo">Classes for the year {data.current_year}</div>
+          <div className="addClassBtn">
+            <AddCircleIcon /> Add class
+          </div>
         </div>
         <div className="classesCardsContainer">
-          {classes.map((c) => (
-            <Link
-              to={`/admin/classes/${c.id}`}
-              className="classCard link"
-              key={c.id}
-            >
-              <div className="topClass">{c.name}</div>
-              <div className="bottomClass">
-                <span>
-                  <ManIcon /> {c.boys}
-                </span>
-                <span>
-                  <WomanIcon /> {c.girls}
-                </span>
-                <span>Total: {c.total}</span>
-              </div>
-            </Link>
-          ))}
+          {
+            data.classes.length === 0 ?
+            <div className="noClasses">No classes created for the year {data.current_year} </div>:
+            data.classes.map((c) => (
+              <Link
+                to={`/classes/${c.id}`}
+                className="classCard link"
+                key={c.id}
+              >
+                <div className="topClass">{c.name}</div>
+                <div className="bottomClass">
+                  <div className='gender'>
+                    <p>Boys: {c.boys}</p>
+                    <p>Girls: {c.girls}</p>
+                  </div>
+                  <span>Total: {c.total}</span>
+                </div>
+              </Link>
+            ))
+          }
         </div>
       </div>
     </div>
